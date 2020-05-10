487d5a1d848813a907a18aa8675da848915102f4
Michael Bednarek <mbednarek360@protonmail.com>
2020-05-10 14:15:47 -0400

    Update update check

--- polybar/updates.sh
+++ polybar/updates.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-yay -S
-updates=$(yay -Qu | wc -l)
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$(("$updates_arch" + "$updates_aur"))
 
 if [ "$updates" -gt 0 ]; then
     echo "$updates"
