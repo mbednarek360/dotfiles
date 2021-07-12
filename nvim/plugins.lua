d28be5d1df9e62be91e23b4ccb1ec85cca380e2b
Michael Bednarek <mbednarek360@protonmail.com>
2021-07-12 17:27:12 -0400

    speed up telescope

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -47,5 +47,15 @@ return require('packer').startup(function()
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
         config = function() require('telescope').setup{
 		defaults = require('telescope.themes').get_dropdown {
-            mappings={i={['<Esc>'] = 'close'}}}} end}
+            mappings={i={['<Esc>'] = 'close'}},
+            vimgrep_arguments = {
+                'rg',
+                '--color=never',
+                '--no-heading',
+                '--with-filename',
+                '--line-number',
+                '--column',
+                '--smart-case'
+        }}}
+     end}
 end)
